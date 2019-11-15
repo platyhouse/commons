4af854a8bcad815efdd1dfba886852e41a612439
root <root@platyhouse.com>
2019-11-15 09:40:12 +0900

    refs pty_docker_save.sh

--- pty_docker_save.sh
+++ pty_docker_save.sh
@@ -3,18 +3,17 @@
 $containerName = $argv[1];
 if (!$containerName)
 {
-	echo "pty_docker_backup version 1.0\n";
-	echo "Copyright (C) 2019 by KwangHee Yoo, PLATYHOUSE Co.,LTD.\n";
+	echo "pty_docker_save.sh version 1.0\n";
+	echo "Copyright (C) 2019 by cpueblo, PlatyHouse Co.,LTD.\n";
 	echo "Web site: https://www.platyhouse.com/\n\n";
-	echo "Usage ./pty_docker_backup.php <containerName>\n";
+	echo "Usage ./pty_docker_save.sh <containerName>\n";
 	echo "=================================\n";
-	system ("docker ps -a");
+	system ("docker ps -a --size");
         echo "=================================\n";
 	exit;
 }
 
 $date = date("Ymd_His");
-# =========================
 $cmd = "docker commit -p {$containerName} {$containerName}:{$date}";
 echo "# {$cmd}\n";; system ($cmd);
 
